36a969039d4695c01d8b31d713d413228b042422
MuzammilCk <muzammilck42@gmail.com>
2026-04-10 20:23:17 +0530

    network address added

--- hadi-perfumes-api/src/main.ts
+++ hadi-perfumes-api/src/main.ts
@@ -15,10 +15,10 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
+    origin: true,
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
